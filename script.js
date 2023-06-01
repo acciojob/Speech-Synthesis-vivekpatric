77553bbae37bc949858d91f168c779a8e66a5df7
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-06-01 20:19:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Your script here.
+    const synth = window.speechSynthesis;
+
+    // Populate the voice dropdown with available voices
+    const voicesDropdown = document.querySelector('#voice');
+    const voices = synth.getVoices();
+    voicesDropdown.innerHTML = voices
+      .filter(voice => voice.lang.includes('en'))
+      .map(voice => `<option value="${voice.name}">${voice.name}</option>`)
+      .join('');
+
+    // Start speaking when the start button is clicked
+    document.querySelector('#start').addEventListener('click', () => {
+      const text = document.querySelector('#text').value;
+      const voice = voices.find(voice => voice.name === voicesDropdown.value);
+      const utterance = new SpeechSynthesisUtterance(text);
+      utterance.voice = voice;
+      synth.speak(utterance);
+    });
+
+    // Stop speaking when the stop button is clicked
+    document.querySelector('#stop').addEventListener('click', () => {
+      synth.cancel();
+    });
